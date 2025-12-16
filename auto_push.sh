b5ac3a0e8e0c8a03cb2b888b544fe80d4f48bd65
fullstack <hswhsw5053@gmail.com>
2025-12-16 18:30:01 +0900

    Auto save 2025-12-16 18:30:01

--- auto_push.sh
+++ auto_push.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-# auto_push_5min.sh
-# 매 5분 정각 0초에만 변경 확인 → 자동 커밋 + 푸시
+# auto_push_5min_win.sh
+# 윈도우 환경(Git Bash/WSL)에서 매 5분 정각 0초에만 변경 확인 → 자동 커밋 + 푸시
+
+# Git 경고 방지: core.autocrlf false 권장
+# git config --global core.autocrlf false
 
 while true; do
     # 현재 분과 초
     min=$(date +%M)
     sec=$(date +%S)
 
-    # 5분 단위 정각 0초가 될 때까지 대기
+    # 다음 5분 정각 0초까지 대기
     sleep_sec=$(( (5 - 10#$min % 5) * 60 - 10#$sec ))
     if [ $sleep_sec -lt 0 ]; then
         sleep_sec=$((sleep_sec + 300))
