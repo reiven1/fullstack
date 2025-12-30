515caaab951f426d751f527b9195c73a10957caa
fullstack <hswhsw5053@gmail.com>
2025-12-30 11:28:27 +0900

    2025-12-30

---
+++ eclipse-workspace/.metadata/.plugins/org.eclipse.core.resources/.history/46/b0cbece326e500101651ce84cd6aa7ba
@@ -0,0 +1,7 @@
+package com.git.main;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
--- eclipse-workspace/gittest/src/com/git/main/Main.java
+++ eclipse-workspace/gittest/src/com/git/main/Main.java
@@ -2,6 +2,6 @@ package com.git.main;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		System.out.println("123");
 	}
 }
